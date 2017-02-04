f89e247d830ec270c217e8c4d91e5d6468faaf22
ajbater <amanda22@hotmail.co.uk>
2017-02-04 17:47:26 +0000

    creates reducers and App component

---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,28 @@
+export default function() {
+  return [
+    {
+      id: 1,
+      first: 'Amanda',
+      last: 'Bater',
+      age: 29,
+      description: 'Learning React and Redux',
+      thumbnail: 'http://dreamatico.com/data_images/kitten/kitten-2.jpg'
+    },
+    {
+      id: 2,
+      first: 'Lionel',
+      last: 'Cat',
+      age: 1,
+      description: 'Learning React and Redux',
+      thumbnail: 'http://www.tehcute.com/pics/201110/marshmellow-kitten-big.jpg'
+    },
+    {
+      id: 3,
+      first: 'Another',
+      last: 'Cat',
+      age: 2,
+      description: 'Learning React and Redux',
+      thumbnail: 'http://freshpet.com/wp-content/uploads/2015/04/kitten.jpg'
+    }
+  ]
+}
